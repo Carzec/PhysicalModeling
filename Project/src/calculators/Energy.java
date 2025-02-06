6655cfefdd6c960315498506f3f143c9d95c4a9f
Carzec <carranzajonathan11@gmail.com>
2025-02-06 14:36:29 -0500

    Abid's initial skeleton

--- Project/src/calculators/Energy.java
+++ Project/src/calculators/Energy.java
@@ -1,5 +1,7 @@
 package calculators;
 
 public class Energy {
-
+    public static double kineticEnergy(double mass, double velocity) {
+        return 0.5 * mass * velocity * velocity;
+    }
 }
---
+++ Project/src/calculators/Momentum.java
@@ -0,0 +1,7 @@
+package calculators;
+
+public class Momentum {
+    public static double calculateMomentum(double mass, double velocity) {
+        return mass * velocity;
+    }
+}
